23d19c1c22a39e2f673d72e6459ac3bb8dd6bdf4
Katie Rey <96268293+katie-rey@users.noreply.github.com>
2023-05-05 22:16:08 +1200

    Add Firebase and send a POST request

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,9 +1,19 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetUp() {
+  function addMeetupHandler(meetupData) {
+    fetch('https://react-meetups-c7d0d-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+
   return (
     <div className="new">
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </div>
   )
 }
